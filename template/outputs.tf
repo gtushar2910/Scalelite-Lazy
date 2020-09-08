68916ac14cc89dda0ad19db377ec0ce4278f0be6
Prof. Tushar Gohil <goheltushar1401@gmail.com>
2020-09-08 14:42:28 +0530

    t

--- template/outputs.tf
+++ template/outputs.tf
@@ -1,10 +1,10 @@
-# output "bigbluebutton_instance_private_ip" {
-#   value = module.bigbluebutton_instance.*.private_ip
-# }
+output "bigbluebutton_instance_private_ip" {
+  value = module.bigbluebutton_instance.*.private_ip
+}
 
-# output "bigbluebutton_instance_public_ip" {
-#   value = module.bigbluebutton_instance.*.public_ip
-# }
+output "bigbluebutton_instance_public_ip" {
+  value = module.bigbluebutton_instance.*.public_ip
+}
 
 output "scalelite_instance_private_ip" {
   value = module.scalelite_instance.private_ip
